5e648d69967e2da7943caf6ecc8bd8fcd400c7b1
Ogunsola Toluwase Paul <oluwaseyiogunsola90@gmail.com>
2026-09-10 21:50:29 +0100

    Clean up PostgreSQL database dump settings
    Removed unnecessary settings and restrictions from the PostgreSQL dump.

--- schema.sql
+++ schema.sql
@@ -2,22 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict JAYd5WShn4Frbnh8BydktaFSp420oOBgifoW2FwVN7X2bD1zN8E2CW8g4yOq2WH
 
--- Dumped from database version 17.6
--- Dumped by pg_dump version 18.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: public; Type: SCHEMA; Schema: -; Owner: -
@@ -4638,5 +4623,5 @@ ALTER TABLE ONLY public.workspaces
 -- PostgreSQL database dump complete
 --
 
-\unrestrict JAYd5WShn4Frbnh8BydktaFSp420oOBgifoW2FwVN7X2bD1zN8E2CW8g4yOq2WH
+
 
